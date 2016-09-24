ca61024fa6f9e72c02b05aca9278fed9e3053196
deepakvaishnav <mailtodeepakvaishnav11@gmail.com>
2016-09-24 14:24:26 +0530

    My first visualisation commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$ (document).ready(function(){
+  //console.log('Document is ready.');
+
+  //I am going to write some code that makes a
+  //div change color when it is clicked
+  $('#click').click(function(){
+
+      console.log('I clicked the div');
+
+      $(".ball").css("background","yellow");
+
+
+  });
+
+
+});
